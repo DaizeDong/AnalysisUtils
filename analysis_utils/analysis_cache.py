3d5f14f3215010adc9a9c8268f792728ef80b885
Daize Dong <dzdong2019@gmail.com>
2025-02-27 18:11:22 +0800

    Update Cache Saving Log

--- analysis_utils/analysis_cache.py
+++ analysis_utils/analysis_cache.py
@@ -23,23 +23,27 @@ def save_analysis_cache_single_batch(save_static=True, reset_cache=True):
 
     if ANALYSIS_ENABLED:  # 🔍
         if len(ANALYSIS_CACHE_DYNAMIC) > 0:
-            create_dir(os.path.join(ANALYSIS_SAVE_DIR, "dynamic", f"{PID}"), suppress_errors=True)
-            torch.save(ANALYSIS_CACHE_DYNAMIC, os.path.join(ANALYSIS_SAVE_DIR, "dynamic", f"{PID}", f"{ANALYSIS_CACHE_BATCH_ID[0]}.pt"), pickle_protocol=HIGHEST_PROTOCOL)
+            save_dir = os.path.join(ANALYSIS_SAVE_DIR, "dynamic", f"{PID}")
+            save_file = os.path.join(save_dir, f"{ANALYSIS_CACHE_BATCH_ID[0]}.pt")
+            create_dir(save_dir, suppress_errors=True)
+            torch.save(ANALYSIS_CACHE_DYNAMIC, save_file, pickle_protocol=HIGHEST_PROTOCOL)
             if reset_cache:
                 ANALYSIS_CACHE_DYNAMIC.clear()
+            print(f"[{PID}] Dynamic cache successfully saved to {save_file}.")
         else:
-            print(f"[{PID}] Skip saving the ANALYSIS_CACHE_DYNAMIC as it is empty.")
+            print(f"[{PID}] Skip saving the `ANALYSIS_CACHE_DYNAMIC` as it is empty.")
 
         if save_static:
             if len(ANALYSIS_CACHE_STATIC) > 0:
-                create_dir(os.path.join(ANALYSIS_SAVE_DIR, "static", f"{PID}"), suppress_errors=True)
-                torch.save(ANALYSIS_CACHE_STATIC, os.path.join(ANALYSIS_SAVE_DIR, "static", f"{PID}", f"{ANALYSIS_CACHE_BATCH_ID[0]}.pt"), pickle_protocol=HIGHEST_PROTOCOL)
+                save_dir = os.path.join(ANALYSIS_SAVE_DIR, "static", f"{PID}")
+                save_file = os.path.join(save_dir, f"{ANALYSIS_CACHE_BATCH_ID[0]}.pt")
+                create_dir(save_dir, suppress_errors=True)
+                torch.save(ANALYSIS_CACHE_STATIC, save_file, pickle_protocol=HIGHEST_PROTOCOL)
                 if reset_cache:
                     ANALYSIS_CACHE_STATIC.clear()
+                print(f"[{PID}] Static cache successfully saved to {save_file}.")
             else:
-                print(f"[{PID}] Skip saving the ANALYSIS_CACHE_STATIC as it is empty.")
-
-        ANALYSIS_CACHE_BATCH_ID[0] += 1
+                print(f"[{PID}] Skip saving the `ANALYSIS_CACHE_STATIC` as it is empty.")
 
         save_json(
             {
@@ -50,7 +54,7 @@ def save_analysis_cache_single_batch(save_static=True, reset_cache=True):
             os.path.join(ANALYSIS_SAVE_DIR, "info.json"),
             indent=4,
         )
-        print(f"[{PID}] Analysis cache successfully saved to {ANALYSIS_SAVE_DIR}.")
+        print(f"[{PID}] Analysis json successfully saved to {os.path.join(ANALYSIS_SAVE_DIR, 'info.json')}.")
 
 
 def save_analysis_cache():
@@ -59,16 +63,22 @@ def save_analysis_cache():
 
     if ANALYSIS_ENABLED:  # 🔍
         if len(ANALYSIS_CACHE_DYNAMIC) > 0:
-            create_dir(os.path.join(ANALYSIS_SAVE_DIR, "dynamic"), suppress_errors=True)
-            torch.save(ANALYSIS_CACHE_DYNAMIC, os.path.join(ANALYSIS_SAVE_DIR, "dynamic", f"{PID}.pt"), pickle_protocol=HIGHEST_PROTOCOL)
+            save_dir = os.path.join(ANALYSIS_SAVE_DIR, "dynamic", f"{PID}")
+            save_file = os.path.join(save_dir, f"{ANALYSIS_CACHE_BATCH_ID[0]}.pt")
+            create_dir(save_dir, suppress_errors=True)
+            torch.save(ANALYSIS_CACHE_DYNAMIC, save_file, pickle_protocol=HIGHEST_PROTOCOL)
+            print(f"[{PID}] Dynamic cache successfully saved to {save_file}.")
         else:
-            print(f"[{PID}] Skip saving the ANALYSIS_CACHE_DYNAMIC as it is empty.")
+            print(f"[{PID}] Skip saving the `ANALYSIS_CACHE_DYNAMIC` as it is empty.")
 
         if len(ANALYSIS_CACHE_STATIC) > 0:
-            create_dir(os.path.join(ANALYSIS_SAVE_DIR, "static"), suppress_errors=True)
-            torch.save(ANALYSIS_CACHE_STATIC, os.path.join(ANALYSIS_SAVE_DIR, "static", f"{PID}.pt"), pickle_protocol=HIGHEST_PROTOCOL)
+            save_dir = os.path.join(ANALYSIS_SAVE_DIR, "static", f"{PID}")
+            save_file = os.path.join(save_dir, f"{ANALYSIS_CACHE_BATCH_ID[0]}.pt")
+            create_dir(save_dir, suppress_errors=True)
+            torch.save(ANALYSIS_CACHE_STATIC, save_file, pickle_protocol=HIGHEST_PROTOCOL)
+            print(f"[{PID}] Static cache successfully saved to {save_file}.")
         else:
-            print(f"[{PID}] Skip saving the ANALYSIS_CACHE_STATIC as it is empty.")
+            print(f"[{PID}] Skip saving the `ANALYSIS_CACHE_STATIC` as it is empty.")
 
         save_json(
             {
@@ -79,4 +89,4 @@ def save_analysis_cache():
             os.path.join(ANALYSIS_SAVE_DIR, "info.json"),
             indent=4,
         )
-        print(f"[{PID}] Analysis cache successfully saved to {ANALYSIS_SAVE_DIR}.")
+        print(f"[{PID}] Analysis json successfully saved to {os.path.join(ANALYSIS_SAVE_DIR, 'info.json')}.")
